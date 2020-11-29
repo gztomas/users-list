dafcb7140eeaec6ce1ef482938b45202382b9662
Tomás González <tgonzalez@asapp.com>
2020-11-29 02:14:04 -0300

    add map markers

--- src/components/Mapbox.ts
+++ src/components/Mapbox.ts
@@ -2,17 +2,16 @@ import styled from "@emotion/styled";
 import mapboxgl, { LngLatBoundsLike, LngLatLike } from "mapbox-gl";
 import * as React from "react";
 
-const accessToken =
+const ACCESS_TOKEN =
   "pk.eyJ1IjoiZ3p0b21hcyIsImEiOiJja2h6OGZuczkwaXNzMnNsMmw3dnk5bmxzIn0.QO9AD8ynVhAuNnjCKGLzVw";
-const mapboxStyle = "mapbox://styles/gztomas/cki0qq81t40f619n23yfhor92";
+const MAPBOX_STYLE = "mapbox://styles/gztomas/cki0qq81t40f619n23yfhor92";
 
 export const geocode = async (query: string) => {
-  const uri = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${accessToken}`;
+  const uri = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${ACCESS_TOKEN}`;
   const result = await fetch(uri);
-  const json = (await result.json()) as {
+  return (await result.json()) as {
     features: ({ bbox: LngLatBoundsLike; center: LngLatLike } | undefined)[];
   };
-  return json;
 };
 
 export const Mapbox = styled.div`
@@ -37,34 +36,42 @@ export const Mapbox = styled.div`
 export const useMapbox = (query: string | null) => {
   const ref = React.useRef<HTMLDivElement>(null);
   const mapboxRef = React.useRef<mapboxgl.Map>();
+  const markerRef = React.useRef<mapboxgl.Marker>();
 
   React.useEffect(() => {
-    const element = ref.current;
-    if (element && !element.children.length) {
-      const container = document.createElement("div");
-      const { style } = container;
-      // These styles are needed inline so mapbox can pick them
-      style.position = "absolute";
-      style.top = style.right = style.bottom = style.left = "0";
-      element.appendChild(container);
-      mapboxRef.current = new mapboxgl.Map({
-        accessToken,
-        container,
-        style: mapboxStyle,
-      });
-    }
-  });
-
-  React.useEffect(() => {
-    const locate = async () => {
+    const update = async () => {
       if (!query) return;
       const location = (await geocode(query)).features[0];
       if (location) {
+        const element = ref.current;
+        if (element && !element.children.length) {
+          const container = document.createElement("div");
+          const { style } = container;
+          // These styles are needed inline so mapbox can pick them
+          style.position = "absolute";
+          style.top = style.right = style.bottom = style.left = "0";
+          element.appendChild(container);
+          mapboxRef.current = new mapboxgl.Map({
+            accessToken: ACCESS_TOKEN,
+            container,
+            style: MAPBOX_STYLE,
+            center: location.center,
+            zoom: 10,
+          });
+          markerRef.current = new mapboxgl.Marker()
+            .setLngLat(location.center)
+            .addTo(mapboxRef.current);
+        }
         mapboxRef.current?.setCenter(location.center);
-        mapboxRef.current?.setMaxBounds(location.bbox);
+        markerRef.current?.setLngLat(location.center);
+        if (location.bbox) {
+          mapboxRef.current?.fitBounds(location.bbox);
+        } else {
+          mapboxRef.current?.setZoom(13);
+        }
       }
     };
-    const timeout = setTimeout(() => void locate(), 500);
+    const timeout = setTimeout(() => void update(), 500);
     return () => clearTimeout(timeout);
   }, [query]);
 
