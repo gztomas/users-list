5bbc2bcf5b9566c7dddc3bc66f15d4528c26aa6e
Tomás González <tgonzalez@asapp.com>
2020-11-30 09:42:53 -0300

    fix geocode test

--- src/components/Mapbox.test.ts
+++ src/components/Mapbox.test.ts
@@ -1,4 +1,5 @@
-import { ACCESS_TOKEN, geocode } from "./Mapbox";
+import { MAPBOX_ACCESS_TOKEN } from "../constants";
+import { geocode } from "./Mapbox";
 
 const fetchMock = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve({}) } as Response)
@@ -7,6 +8,6 @@ test("geocode", async () => {
   window.fetch = fetchMock;
   await geocode("query");
   expect(fetchMock).toHaveBeenCalledWith(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/query.json?access_token=${ACCESS_TOKEN}`
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/query.json?access_token=${MAPBOX_ACCESS_TOKEN}`
   );
 });
